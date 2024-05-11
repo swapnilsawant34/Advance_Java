fe6eff981b40ca8ed14fed6257c5300391b1037d
swapnilsawant34 <168524049+swapnilsawant34@users.noreply.github.com>
2024-05-11 21:02:04 +0530

    Session Implementation using servlet

---
+++ Session_Servlet/src/main/java/session_servlet/create_session.java
@@ -0,0 +1,38 @@
+package session_servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class create_session
+ */
+@WebServlet("/save")
+public class create_session extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		HttpSession session=request.getSession(true);
+		String username=request.getParameter("name");
+		String password=request.getParameter("pass");
+		session.setAttribute("u", username);
+		session.setAttribute("p", password);
+		out.println("<a href='View'>View Session Data</a>");
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Session_Servlet/src/main/java/session_servlet/view_session.java
@@ -0,0 +1,38 @@
+package session_servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class view_session
+ */
+@WebServlet("/View")
+public class view_session extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		HttpSession session=request.getSession();
+		String username=session.getAttribute("u").toString();
+		String password=session.getAttribute("p").toString();
+		out.println("<h1> Data From Session</h1>");
+		out.println("<h1>Username is &nbsp;"+username+"</h1>");
+		out.println("<h1>Password is &nbsp;"+password+"</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
